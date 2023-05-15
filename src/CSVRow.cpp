4630f7ddb770e54ea46706335a93c11dbf8c9453
Christian Opperman <christian.opperman@gmail.com>
2023-05-14 20:25:18 -0400

    Changing CSVRow::SplitRow method to properly handle fields with commas in them, delineated by quotes

--- src/CSVRow.cpp
+++ src/CSVRow.cpp
@@ -20,7 +20,8 @@ std::string CSVRow::getChargeType() { return charge_type; }
 std::string CSVRow::getAmount() { return amount; }
 std::string CSVRow::getPayee() { return payee; }
 
-// Setter methods
+/// @brief 
+/// @param payee_map 
 void CSVRow::setPayee(std::unordered_map<std::string, std::string> payee_map) { 
     for ( const auto [key, value] : payee_map ) {
         if (std::regex_search(merchant, std::regex(key))) {
@@ -31,30 +32,51 @@ void CSVRow::setPayee(std::unordered_map<std::string, std::string> payee_map) {
         payee = GetPayee();
     }
     }
-        
+
+
 void CSVRow::printRow() {
     std::cout << date << ", " << merchant << ", " << amount << ", " << payee << "\n";
 }
 
+/// @brief Takes a CSV row and parses it into tokens. This algorithm handles fields with commas in them, provided they are delineated by quotes.
+/// @param unsplit_row 
+/// @param delim 
+/// @return <std::vector<std::string>> row_tokens, a vector of strings representing the parsed tokens from the CSV row.
 std::vector<std::string> CSVRow::SplitRow(std::string& unsplit_row, const char& delim) {
-    // Remove any carriage returns from the row
-    unsplit_row.erase(std::remove(unsplit_row.begin(), unsplit_row.end(), '\r'), unsplit_row.end());
-    std::istringstream stream(unsplit_row);
-    std::string token;
+    bool quote_flag = false;
+    std::vector<std::string> row_tokens;
+    std::string token = "";
 
-    std::vector<std::string> row;
-
-    while(getline(stream, token, delim)) {
-        row.push_back(token);
+    for ( std::string::iterator it=unsplit_row.begin(); it!=unsplit_row.end(); ++it) {
+        if (*it == delim && !quote_flag) {
+            row_tokens.push_back(token);
+            token = "";
+        } else if (*it == '\"' && !quote_flag) {
+            quote_flag = true;
+            token += *it;
+        } else if (*it == '\"' && quote_flag) {
+            quote_flag = false;
+            token += *it;
+        } else if (*it == '\r' || *it == '\n') {
+            row_tokens.push_back(token);
+            break;
+        } else {
+            token += *it;
+        }
     }
+    // Necessary to add the final token at the end of the file (as the file doesn't terminate with a carriage return or comma)
+    row_tokens.push_back(token);
 
-    return row;
+    return row_tokens;
 }
 
+/// @brief 
+/// @return 
 std::string CSVRow::GetPayee() {
     // Prompt the user to supply the payer for an unknown merchant
     std::string user_input;
     std::cout << "Please provide the payee for " << date << "/" << merchant << "/" << amount << ": ";
+    // TODO: Add error handling into function to prevent non-valid values from being passed in by user
     std::cin >> user_input;
     return user_input;
 }
